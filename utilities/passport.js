5123c0eb2c0c19dd11b89f47b77c86db3d24790e
abasud <doctorbermeo@gmail.com>
2026-03-31 20:34:50 -0500

    Add proxy to Google strategy

--- utilities/passport.js
+++ utilities/passport.js
@@ -7,7 +7,8 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
   },
   (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
